12516ed6ea4d8953b844fdd6a76d0f9d55ae5012
moxas3d <moxas3d@gmail.com>
2026-07-19 17:38:52 +0300

    Adding staging url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-const fallbackApiBaseUrl = "http://192.168.1.68:8087/facility_mobile_api/v1";
+const fallbackApiBaseUrl = "https://kamahtech-t-facility-s1-35121300.dev.odoo.com/facility_mobile_api/v1";
 
 export const API_BASE_URL =
   process.env.EXPO_PUBLIC_API_BASE_URL || fallbackApiBaseUrl;
